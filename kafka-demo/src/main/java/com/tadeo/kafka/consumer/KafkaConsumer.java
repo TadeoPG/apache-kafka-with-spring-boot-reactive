4eb15587b8472b46988600753d9b3d55392e7f59
TadeoPG <tadeomanuelportillo@gmail.com>
2024-10-09 11:53:03 -0500

    feat(json-producer & consumer): add producer and consumer for json

--- kafka-demo/src/main/java/com/tadeo/kafka/consumer/KafkaConsumer.java
+++ kafka-demo/src/main/java/com/tadeo/kafka/consumer/KafkaConsumer.java
@@ -1,16 +1,24 @@
 package com.tadeo.kafka.consumer;
 
 
+import com.tadeo.kafka.payload.Student;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@KafkaListener(topics = "tadeo", groupId = "myGroup")
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "tadeo", groupId = "myGroup")
+    @KafkaHandler
     public void consumeMessage(String message) {
-        log.info(String.format("#### -> Consumed message from tadeo Topic -> %s", message));
+        log.info(String.format("#### -> Consumed string message from tadeo Topic -> %s", message));
+    }
+
+    @KafkaHandler
+    public void consumeJsonMessage(Student student) {
+        log.info(String.format("#### -> Consumed Student message from tadeo Topic -> %s", student));
     }
 }
